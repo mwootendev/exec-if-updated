6f2cb07a8618734e2f2b44d64acb64b85b1dfb12
Michael Wooten <mwooten.dev@gmail.com>
2018-06-27 16:43:17 -0400

    Adding initial version of the utility.

---
+++ exec-if-updated.js
@@ -0,0 +1,72 @@
+const yargs = require('yargs');
+const globby = require('globby');
+const execa = require('execa');
+
+const options = yargs
+  .example('$0 --source src/** --source assets/** --target dist/** npm run build')
+  .option('s', {
+    alias: 'source',
+    demandOption: true,
+    describe: 'A glob describing the source files that may be updated. May be supplied more than once for additional source files.',
+    type: 'array',
+    nargs: 1
+  })
+  .option('t', {
+    alias: 'target',
+    demandOption: true,
+    describe: 'A glob describing the target files to which the source files will be compared. May be supplied more than once for additional target files.',
+    type: 'array',
+    nargs: 1
+  })
+  .wrap(yargs.terminalWidth())
+  .argv;
+
+main(options);
+
+async function main(options) {
+  const sourceFiles = await matchFiles(options.source);
+  const targetFiles = await matchFiles(options.target);
+
+  if (isSourceUpdated(sourceFiles, targetFiles)) {
+    try {
+      const command = parseCommand(options);
+      const childProcess = execa.shell(command);
+      childProcess.stdout.pipe(process.stdout);
+      childProcess.stderr.pipe(process.stderr);
+      process.stdin.pipe(childProcess.stdin);
+      await childProcess;
+    } catch (e) {
+      console.error(e.message);
+      process.exit(1);
+    }    
+  } else {
+    process.exit();
+  }
+}
+
+function matchFiles(fileGlob) {
+  const GLOB_OPTIONS = {
+    stats: true
+  };
+  return globby(fileGlob, GLOB_OPTIONS);
+}
+
+function isSourceUpdated(sourceFiles, targetFiles) {
+  const toModifiedDate = (file => new Date(file.mtimeMs));
+  const toLatestModified = (latestModifiedTime, fileModifiedTime) => fileModifiedTime.getTime() > latestModifiedTime.getTime() ?
+    fileModifiedTime :
+    latestModifiedTime;
+
+  const latestSourceDate = sourceFiles.map(toModifiedDate).reduce(toLatestModified);
+  const latestTargetDate = targetFiles.map(toModifiedDate).reduce(toLatestModified);
+
+  return latestSourceDate.getTime() > latestTargetDate.getTime();
+}
+
+function parseCommand(options) {
+ if (options._ && options._.length) {
+     return options._.join(' ');
+  } else {
+    throw new Error('Missing command');
+  }
+}
